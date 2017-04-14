b2c9fb0e6770b812d01b1b41961a9b12c034ab0f
Sherafgan <kssfortis@gmail.com>
2017-04-14 21:06:45 +0300

    fixed issue with negative values of vectors

--- similarity-database-server/src/main/java/com/vectorization/parsing/ServerLexer.java
+++ similarity-database-server/src/main/java/com/vectorization/parsing/ServerLexer.java
@@ -23,107 +23,136 @@ import com.vectorization.parsing.Token.Type;
 
 public class ServerLexer extends Lexer {
 
-	public enum SSType implements Type {
-		NUMBER, NAME,STAR, COMMA, DOT, EQUALS, LBRACK, RBRACK, EOF_TYPE
-	}
+    public enum SSType implements Type {
+        NUMBER, NEGNUMBER, NAME, STAR, COMMA, DOT, EQUALS, LBRACK, RBRACK, EOF_TYPE
+    }
 
-	@Inject
-	public ServerLexer(@Assisted String input) {
-		super(input);
-	}
+    @Inject
+    public ServerLexer(@Assisted String input) {
+        super(input);
+    }
 
-	private boolean isPoint() {
-		return getLookAhead() == '.';
-	}
+    private boolean isPoint() {
+        return getLookAhead() == '.';
+    }
 
-	private boolean isComma() {
-		return getLookAhead() == ',';
-	}
+    private boolean isComma() {
+        return getLookAhead() == ',';
+    }
 
-	private boolean isLBrack() {
-		return getLookAhead() == '[';
-	}
+    private boolean isLBrack() {
+        return getLookAhead() == '[';
+    }
 
-	private boolean isRBrack() {
-		return getLookAhead() == ']';
-	}
+    private boolean isRBrack() {
+        return getLookAhead() == ']';
+    }
 
-	private boolean isEquals() {
-		return getLookAhead() == '=';
-	}
-	
-	private boolean isStar(){
-		return getLookAhead() == '*';
-	}
+    private boolean isEquals() {
+        return getLookAhead() == '=';
+    }
 
-	private boolean isDigit() {
-		return Character.isDigit(getLookAhead());
-	}
+    private boolean isStar() {
+        return getLookAhead() == '*';
+    }
 
-	private Token consumeName() {
-		StringBuilder sb = new StringBuilder();
-		if (isIdentifierStart()) {
-			sb.append(getLookAhead());
-			consume();
-		}
+    private boolean isDigit() {
+        return Character.isDigit(getLookAhead());
+    }
 
-		while (Character.isJavaIdentifierPart(getLookAhead())) {
-			sb.append(getLookAhead());
-			consume();
-		}
-		return new Token(SSType.NAME, sb.toString());
-	}
+    private boolean isNeg() {
+        return getLookAhead() == '-';
+    }
 
-	private boolean isIdentifierStart() {
-		return Character.isJavaIdentifierStart(getLookAhead());
-	}
+    private Token consumeName() {
+        StringBuilder sb = new StringBuilder();
+        if (isIdentifierStart()) {
+            sb.append(getLookAhead());
+            consume();
+        }
 
-	private Token consumeNumber() {
-		StringBuilder sb = new StringBuilder();
-		while (isDigit()) {
-			sb.append(getLookAhead());
-			consume();
-		}
-		if (isPoint()) {
-			sb.append(getLookAhead());
-			consume();
-		}
-		while (isDigit()) {
-			sb.append(getLookAhead());
-			consume();
-		}
-		return new Token(SSType.NUMBER, sb.toString());
-	}
+        while (Character.isJavaIdentifierPart(getLookAhead())) {
+            sb.append(getLookAhead());
+            consume();
+        }
+        return new Token(SSType.NAME, sb.toString());
+    }
 
-	@Override
-	public Token next() {
-		consumeWhiteSpace();
-		if (isLBrack()) {
-			consume();
-			return new Token(SSType.LBRACK, "[");
-		}
-		if (isRBrack()) {
-			consume();
-			return new Token(SSType.RBRACK, "]");
-		}
-		if (isComma()) {
-			consume();
-			return new Token(SSType.COMMA, ",");
-		}
-		if (isPoint()) {
-			consume();
-			return new Token(SSType.DOT, ".");
-		}
-		if (isEquals()) {
-			consume();
-			return new Token(SSType.EQUALS, "=");
-		}
-		if (isStar()) {
-			consume();
-			return new Token(SSType.STAR, "*");
-		}
-		if (isDigit()) return consumeNumber();
-		if (isIdentifierStart()) return consumeName();
-		return new Token(SSType.EOF_TYPE, "");
-	}
+    private boolean isIdentifierStart() {
+        return Character.isJavaIdentifierStart(getLookAhead());
+    }
+
+    private Token consumeNumber() {
+        StringBuilder sb = new StringBuilder();
+        while (isDigit()) {
+            sb.append(getLookAhead());
+            consume();
+        }
+        if (isPoint()) {
+            sb.append(getLookAhead());
+            consume();
+        }
+        while (isDigit()) {
+            sb.append(getLookAhead());
+            consume();
+        }
+        return new Token(SSType.NUMBER, sb.toString());
+    }
+
+    private Token consumeNegNumber() {
+        StringBuilder sb = new StringBuilder();
+        while (isDigit() || isNeg()) {
+            sb.append(getLookAhead());
+            consume();
+        }
+        if (isPoint()) {
+            sb.append(getLookAhead());
+            consume();
+        }
+        while (isDigit() || isNeg()) { //get rid of '|| isNeg'
+            sb.append(getLookAhead());
+            consume();
+        }
+        String sbString = sb.toString();
+        return new Token(SSType.NEGNUMBER, sbString);
+    }
+
+    @Override
+    public Token next() {
+        consumeWhiteSpace();
+        if (isLBrack()) {
+            consume();
+            return new Token(SSType.LBRACK, "[");
+        }
+        if (isRBrack()) {
+            consume();
+            return new Token(SSType.RBRACK, "]");
+        }
+        if (isComma()) {
+            consume();
+            return new Token(SSType.COMMA, ",");
+        }
+        if (isPoint()) {
+            consume();
+            return new Token(SSType.DOT, ".");
+        }
+        if (isEquals()) {
+            consume();
+            return new Token(SSType.EQUALS, "=");
+        }
+        if (isStar()) {
+            consume();
+            return new Token(SSType.STAR, "*");
+        }
+        if (isNeg()) {
+            return consumeNegNumber();
+        }
+        if (isDigit()) {
+            return consumeNumber();
+        }
+        if (isIdentifierStart()) {
+            return consumeName();
+        }
+        return new Token(SSType.EOF_TYPE, "");
+    }
 }
--- similarity-database-server/src/main/java/com/vectorization/parsing/ServerParser.java
+++ similarity-database-server/src/main/java/com/vectorization/parsing/ServerParser.java
@@ -17,9 +17,6 @@
  */
 package com.vectorization.parsing;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.google.inject.Inject;
 import com.google.inject.assistedinject.Assisted;
 import com.vectorization.core.Vector;
@@ -30,247 +27,260 @@ import com.vectorization.server.Processor;
 import com.vectorization.server.command.CommandFactory;
 import com.vectorization.server.security.Security;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ServerParser extends Parser<ServerCommand> {
 
-	private Processor processor;
-	private CommandFactory factory;
-	private Security security;
+    private Processor processor;
+    private CommandFactory factory;
+    private Security security;
 
-	@Inject
-	public ServerParser(Security security, CommandFactory factory,
-			@Assisted Processor processor, @Assisted Lexer l) {
-		super(l);
-		this.security = security;
-		this.factory = factory;
-		this.processor = processor;
-	}
+    @Inject
+    public ServerParser(Security security, CommandFactory factory,
+                        @Assisted Processor processor, @Assisted Lexer l) {
+        super(l);
+        this.security = security;
+        this.factory = factory;
+        this.processor = processor;
+    }
 
-	@Override
-	public ServerCommand parse() {
-		if (getLookAhead().type.equals(SSType.NAME)) {
-			if (getLookAhead().val.equals("login")) {
-				return login();
-			}
-			if (getLookAhead().val.equals("change")) {
-				return passwd();
-			}
-			if (getLookAhead().val.equals("add")) {
-				return addUser();
-			}
-			if (getLookAhead().val.equals("grant")) {
-				return grant();
-			}
-			if (getLookAhead().val.equals("use")) {
-				return use();
-			}
-			if (getLookAhead().val.equals("create")) {
-				return create();
-			}
-			if (getLookAhead().val.equals("drop")) {
-				return drop();
-			}
-			if (getLookAhead().val.equals("find")) {
-				return find();
-			}
-			if (getLookAhead().val.equals("insert")) {
-				return insert();
-			}
-			if (getLookAhead().val.equals("remove")) {
-				return remove();
-			}
-			if (getLookAhead().val.equals("list")) {
-				return list();
-			}
-			if (getLookAhead().val.equals("show")) {
-				return show();
-			}
-		}
-		return factory.newNullCommand("No such command");
-	}
+    @Override
+    public ServerCommand parse() {
+        if (getLookAhead().type.equals(SSType.NAME)) {
+            if (getLookAhead().val.equals("login")) {
+                return login();
+            }
+            if (getLookAhead().val.equals("change")) {
+                return passwd();
+            }
+            if (getLookAhead().val.equals("add")) {
+                return addUser();
+            }
+            if (getLookAhead().val.equals("grant")) {
+                return grant();
+            }
+            if (getLookAhead().val.equals("use")) {
+                return use();
+            }
+            if (getLookAhead().val.equals("create")) {
+                return create();
+            }
+            if (getLookAhead().val.equals("drop")) {
+                return drop();
+            }
+            if (getLookAhead().val.equals("find")) {
+                return find();
+            }
+            if (getLookAhead().val.equals("insert")) {
+                return insert();
+            }
+            if (getLookAhead().val.equals("remove")) {
+                return remove();
+            }
+            if (getLookAhead().val.equals("list")) {
+                return list();
+            }
+            if (getLookAhead().val.equals("show")) {
+                return show();
+            }
+        }
+        return factory.newNullCommand("No such command");
+    }
 
-	private ServerCommand passwd() {
-		match(SSType.NAME, "change");
-		match(SSType.NAME, "password");
-		match(SSType.NAME, "to");
-		String password = name();
-		return factory.newChangePasswordCommand(security, password);
-	}
+    private ServerCommand passwd() {
+        match(SSType.NAME, "change");
+        match(SSType.NAME, "password");
+        match(SSType.NAME, "to");
+        String password = name();
+        return factory.newChangePasswordCommand(security, password);
+    }
 
-	private ServerCommand grant() {
-		match(SSType.NAME, "grant");
+    private ServerCommand grant() {
+        match(SSType.NAME, "grant");
 
-		List<String> permissions = new ArrayList<String>();
-		// either a star
-		if (getLookAhead().type.equals(SSType.STAR)) {
-			match(SSType.STAR);
-			permissions.add("*");
-		}
-		// or list of permissions
-		else {
-			String permission = name();
-			permissions.add(permission);
-			while (getLookAhead().type.equals(SSType.COMMA)) {
-				match(SSType.COMMA);
-				permission = name();
-				permissions.add(permission);
-			}
-		}
-		String dbName = "*";
-		String spaceName = "*";
-		if (getLookAhead().val.equals("on")) {
-			match(SSType.NAME, "on");
+        List<String> permissions = new ArrayList<String>();
+        // either a star
+        if (getLookAhead().type.equals(SSType.STAR)) {
+            match(SSType.STAR);
+            permissions.add("*");
+        }
+        // or list of permissions
+        else {
+            String permission = name();
+            permissions.add(permission);
+            while (getLookAhead().type.equals(SSType.COMMA)) {
+                match(SSType.COMMA);
+                permission = name();
+                permissions.add(permission);
+            }
+        }
+        String dbName = "*";
+        String spaceName = "*";
+        if (getLookAhead().val.equals("on")) {
+            match(SSType.NAME, "on");
 
-			if (getLookAhead().type.equals(SSType.STAR)) {
-				match(SSType.STAR);
-			} else {
-				dbName = name();
-			}
+            if (getLookAhead().type.equals(SSType.STAR)) {
+                match(SSType.STAR);
+            } else {
+                dbName = name();
+            }
 
-			if (getLookAhead().type.equals(SSType.DOT)) {
-				match(SSType.DOT);
-				if (getLookAhead().type.equals(SSType.STAR)) {
-					match(SSType.STAR);
-				} else {
-					spaceName = name();
-				}
-			}
-		}
-		match(SSType.NAME, "to");
-		String username = name();
-		return factory.newGrantCommand(security, permissions, dbName,
-				spaceName, username);
-	}
+            if (getLookAhead().type.equals(SSType.DOT)) {
+                match(SSType.DOT);
+                if (getLookAhead().type.equals(SSType.STAR)) {
+                    match(SSType.STAR);
+                } else {
+                    spaceName = name();
+                }
+            }
+        }
+        match(SSType.NAME, "to");
+        String username = name();
+        return factory.newGrantCommand(security, permissions, dbName,
+                spaceName, username);
+    }
 
-	private ServerCommand addUser() {
-		match(SSType.NAME, "add");
-		match(SSType.NAME, "user");
-		String username = name();
-		match(SSType.NAME, "identified");
-		match(SSType.NAME, "by");
-		String password = name();
-		return factory.newAddUserCommand(security, username, password);
-	}
+    private ServerCommand addUser() {
+        match(SSType.NAME, "add");
+        match(SSType.NAME, "user");
+        String username = name();
+        match(SSType.NAME, "identified");
+        match(SSType.NAME, "by");
+        String password = name();
+        return factory.newAddUserCommand(security, username, password);
+    }
 
-	private ServerCommand login() {
-		match(SSType.NAME, "login");
-		String username = name();
-		match(SSType.NAME, "with");
-		String password = name();
-		return factory.newLoginCommand(username, password);
-	}
+    private ServerCommand login() {
+        match(SSType.NAME, "login");
+        String username = name();
+        match(SSType.NAME, "with");
+        String password = name();
+        return factory.newLoginCommand(username, password);
+    }
 
-	private ServerCommand use() {
-		match(SSType.NAME, "use");
-		String databaseName = name();
-		Database database = factory.newDatabase(databaseName);
-		processor.setDatabase(database);
-		return factory.newUseCommand(databaseName);
-	}
+    private ServerCommand use() {
+        match(SSType.NAME, "use");
+        String databaseName = name();
+        Database database = factory.newDatabase(databaseName);
+        processor.setDatabase(database);
+        return factory.newUseCommand(databaseName);
+    }
 
-	private ServerCommand show() {
-		match(SSType.NAME, "show");
-		String spaceName = "";
-		if (!getLookAhead().type.equals(SSType.EOF_TYPE)) {
-			spaceName = name();
-		}
-		String vectorName = "";
-		if (getLookAhead().type.equals(SSType.DOT)) {
-			match(SSType.DOT);
-			vectorName = name();
-		}
-		return factory.newShowCommand(spaceName, vectorName);
-	}
+    private ServerCommand show() {
+        match(SSType.NAME, "show");
+        String spaceName = "";
+        if (!getLookAhead().type.equals(SSType.EOF_TYPE)) {
+            spaceName = name();
+        }
+        String vectorName = "";
+        if (getLookAhead().type.equals(SSType.DOT)) {
+            match(SSType.DOT);
+            vectorName = name();
+        }
+        return factory.newShowCommand(spaceName, vectorName);
+    }
 
-	private ServerCommand list() {
-		match(SSType.NAME, "list");
-		return factory.newListCommand();
-	}
+    private ServerCommand list() {
+        match(SSType.NAME, "list");
+        return factory.newListCommand();
+    }
 
-	private ServerCommand insert() {
-		match(SSType.NAME, "insert");
-		String vectorName = name();
-		match(SSType.EQUALS);
-		Vector v = vector(vectorName);
-		match(SSType.NAME, "into");
-		String tableName = name();
-		return factory.newInsertCommand(tableName, v);
-	}
+    private ServerCommand insert() {
+        match(SSType.NAME, "insert");
+        String vectorName = name();
+        match(SSType.EQUALS);
+        Vector v = vector(vectorName);
+        match(SSType.NAME, "into");
+        String tableName = name();
+        return factory.newInsertCommand(tableName, v);
+    }
 
-	private ServerCommand remove() {
-		match(SSType.NAME, "remove");
-		String vectorName = name();
-		match(SSType.NAME, "from");
-		String tableName = name();
-		return factory.newRemoveCommand(tableName, vectorName);
-	}
+    private ServerCommand remove() {
+        match(SSType.NAME, "remove");
+        String vectorName = name();
+        match(SSType.NAME, "from");
+        String tableName = name();
+        return factory.newRemoveCommand(tableName, vectorName);
+    }
 
-	private ServerCommand find() {
-		match(SSType.NAME, "find");
-		int k = integer();
-		match(SSType.NAME, "nearest");
-		match(SSType.NAME, "to");
-		Vector v;
-		if (getLookAhead().type.equals(SSType.LBRACK)) {
-			v = vector("");
-			match(SSType.NAME, "in");
-			String spaceName = name();
-			return factory.newFindCommand(spaceName, k, v);
-		} else {
-			String querySpaceName = name();
-			match(SSType.DOT);
-			String queryVectorName = name();
-			match(SSType.NAME, "in");
-			String spaceName = name();
-			return factory.newFindVectorCommand(spaceName, k, querySpaceName,
-					queryVectorName);
-		}
-	}
+    private ServerCommand find() {
+        match(SSType.NAME, "find");
+        int k = integer();
+        match(SSType.NAME, "nearest");
+        match(SSType.NAME, "to");
+        Vector v;
+        if (getLookAhead().type.equals(SSType.LBRACK)) {
+            v = vector("");
+            match(SSType.NAME, "in");
+            String spaceName = name();
+            return factory.newFindCommand(spaceName, k, v);
+        } else {
+            String querySpaceName = name();
+            match(SSType.DOT);
+            String queryVectorName = name();
+            match(SSType.NAME, "in");
+            String spaceName = name();
+            return factory.newFindVectorCommand(spaceName, k, querySpaceName,
+                    queryVectorName);
+        }
+    }
 
-	private ServerCommand create() {
-		match(SSType.NAME, "create");
-		match(SSType.NAME, "space");
-		String spaceName = name();
-		match(SSType.NAME, "with");
-		match(SSType.NAME, "dimensionality");
-		int dimensionality = integer();
-		return factory.newCreateCommand(spaceName, dimensionality);
-	}
+    private ServerCommand create() {
+        match(SSType.NAME, "create");
+        match(SSType.NAME, "space");
+        String spaceName = name();
+        match(SSType.NAME, "with");
+        match(SSType.NAME, "dimensionality");
+        int dimensionality = integer();
+        return factory.newCreateCommand(spaceName, dimensionality);
+    }
 
-	private ServerCommand drop() {
-		match(SSType.NAME, "drop");
-		String spaceName = name();
-		return factory.newDropCommand(spaceName);
-	}
+    private ServerCommand drop() {
+        match(SSType.NAME, "drop");
+        String spaceName = name();
+        return factory.newDropCommand(spaceName);
+    }
 
-	private String name() {
-		String name = getLookAhead().val;
-		match(SSType.NAME);
-		return name;
-	}
+    private String name() {
+        String name = getLookAhead().val;
+        match(SSType.NAME);
+        return name;
+    }
 
-	private int integer() {
-		int integer = Integer.parseInt(getLookAhead().val);
-		match(SSType.NUMBER);
-		return integer;
-	}
+    private int integer() {
+        int integer = Integer.parseInt(getLookAhead().val);
+        match(SSType.NUMBER);
+        return integer;
+    }
 
-	private double real() {
-		double real = Double.parseDouble(getLookAhead().val);
-		match(SSType.NUMBER);
-		return real;
-	}
+    private double real() {
+        double real = Double.parseDouble(getLookAhead().val);
+        match(SSType.NUMBER);
+        return real;
+    }
 
-	private Vector vector(String name) {
-		match(SSType.LBRACK);
-		List<Double> vals = new ArrayList<Double>();
-		while (getLookAhead().type.equals(SSType.NUMBER)) {
-			vals.add(real());
-			if (getLookAhead().type.equals(SSType.COMMA)) {
-				match(SSType.COMMA);
-			}
-		}
-		match(SSType.RBRACK);
-		return Vectors.createVector(name, vals);
-	}
+    private double negreal() {
+        double real = Double.parseDouble(getLookAhead().val);
+        match(SSType.NEGNUMBER);
+        return real;
+    }
+
+    private Vector vector(String name) {
+        match(SSType.LBRACK);
+        List<Double> vals = new ArrayList<Double>();
+        while (getLookAhead().type.equals(SSType.NUMBER) || getLookAhead().type.equals(SSType.NEGNUMBER)) {
+            if (getLookAhead().type.equals(SSType.NUMBER)) {
+                vals.add(real());
+            } else if (getLookAhead().type.equals(SSType.NEGNUMBER)) {
+                vals.add(negreal());
+            }
+            if (getLookAhead().type.equals(SSType.COMMA)) {
+                match(SSType.COMMA);
+            }
+        }
+        match(SSType.RBRACK);
+        return Vectors.createVector(name, vals);
+    }
 
 }
